8b3d9c7329ab90802fd1a8b70ccf5d6a0e8c2d7f
William Porto <william.porto@nave.rs>
2021-08-11 09:15:47 -0300

    chore(app): setup jetpack compose

--- buildSrc/src/main/java/com/powilliam/android/buildsrc/Dependencies.kt
+++ buildSrc/src/main/java/com/powilliam/android/buildsrc/Dependencies.kt
@@ -12,5 +12,27 @@ object Dependencies {
     object AndroidX {
         const val core = "androidx.core:core-ktx:1.6.0"
         const val appCompat = "androidx.appcompat:appcompat:1.3.1"
+
+        object Activity {
+            const val activityCompose = "androidx.activity:activity-compose:1.3.1"
+        }
+
+        object Compose {
+            const val composeVersion = "1.0.1"
+            const val ui = "androidx.compose.ui:ui:$composeVersion"
+            const val foundation = "androidx.compose.foundation:foundation:$composeVersion"
+            const val animation = "androidx.compose.animation:animation:$composeVersion"
+            const val tooling = "androidx.compose.ui:ui-tooling:$composeVersion"
+
+            const val materialDesign = "androidx.compose.material:material:$composeVersion"
+            const val materialDesignIcons = "androidx.compose.material:material-icons-core:$composeVersion"
+            const val materialDesignIconsExtended = "androidx.compose.material:material-icons-extended:$composeVersion"
+
+            const val runtimeLivedata = "androidx.compose.runtime:runtime-livedata:$composeVersion"
+        }
+
+        object Lifecycle {
+            const val viewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07"
+        }
     }
 }
